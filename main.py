ed0078936aceca0861bfdb09991c4493dca0d9ce
TD\tdelforge <tristandelforge1@gmail.com>
2018-04-24 02:43:03 -0500

    started planning, thoughts

---
+++ main.py
@@ -0,0 +1,37 @@
+import copy
+
+
+class Algorithm:
+    def __init__(self, x):
+        self.layers = []
+        self.layers.append([x])
+
+
+class Operation():
+    '''
+    Operations must include splitting, comparison, conditionals, inserting
+
+    inputs must be able to deal with offsets: i +- x or set indexes: i//x
+
+    must report num of operations
+    '''
+
+    def __init__(self, params):
+        pass
+
+
+class SplitOperation(Operation):
+    pass
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
